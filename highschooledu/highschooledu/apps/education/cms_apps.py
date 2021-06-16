f624692dc4b5b1db2c2d2f83be01a24b5b6ca067
amosbaranes <amos@drbaranes.com>
2021-06-16 19:57:38 +0300

    Amos - turn education to app

--- highschooledu/highschooledu/apps/education/cms_apps.py
+++ highschooledu/highschooledu/apps/education/cms_apps.py
@@ -9,7 +9,7 @@ from django.utils.translation import ugettext_lazy as _
 @apphook_pool.register  # register the application
 class EducationApp(CMSApp):
 
-    app_name = 'education'
+    app_name = 'education_app'
 
     name = _('Education')
 
